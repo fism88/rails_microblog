93ec103219b1b7b30611640275ba0c74816c8ba0
Faraaz Ismail <fism88@gmail.com>
2018-03-09 13:19:35 -0600

    Test that invalid login flash message disappears when making new requests; find user by email and authenticate password submitted; if invalid login, use flash.now to render error message for just that page and render login page again; add extra line between login submit and link to register for some breathing room

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  test "login with invalid information" do
+    get login_path
+
+    assert_template 'sessions/new'
+    assert_select 'form[action="/login"]'
+    post login_path, params: { session: { email: 'user@invalid',
+                                          password: 'foo' } }
+
+    assert_template 'sessions/new'
+    assert_select 'form[action="/login"]'
+    assert_not flash.empty?
+
+    get root_path
+    assert flash.empty?
+  end
+end
