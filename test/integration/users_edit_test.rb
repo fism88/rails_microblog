46a56281ca5f661facc826eacd427f5bf8eeeb8e
Faraaz Ismail <fism88@gmail.com>
2018-03-12 10:55:44 -0500

    Test that forwarding url is set before login and deleted after login

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -36,8 +36,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+    assert_not session[:forwarding_url].empty?
     log_in_as(@user)
     assert_redirected_to edit_user_url(@user)
+    assert_not session[:forwarding_url]
 
     patch user_path(@user), params: { user: { name: "Foo Bar",
                                               email: "foo@bar.com",
