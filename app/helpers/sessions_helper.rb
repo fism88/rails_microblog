58091af2f0bae388ad5b1821a0c5159e7c4aafef
Faraaz Ismail <fism88@gmail.com>
2018-03-12 10:29:48 -0500

    extract checking of user being current_user to a session helper method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,14 @@
 module SessionsHelper
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
   end
 
   def logged_in?
