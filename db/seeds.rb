414f9c42d23996b795dd23619f1a39d11f4d6b2c
Faraaz Ismail <fism88@gmail.com>
2018-03-16 10:28:34 -0500

    Typo in db seeding when trying to pull from user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ users = User.order(:created_at).take(6)
 end
 
 # Following relationships
-users = Users.all
+users = User.all
 user = users.first
 following = users[2..50]
 followers = users[3..40]
